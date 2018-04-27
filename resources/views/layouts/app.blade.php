c30d04141a4402e61c5dbcc8594fffbb8442b750
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-04-27 16:18:45 +0800

    Setting up Tabler empty layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,33 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Language" content="en" />
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="theme-color" content="#4188c9">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
+    <!-- Dashboard Core -->
+    <link href="{{ asset('tabler/assets/css/dashboard.css') }}" rel="stylesheet" />
+    <!-- Google Maps Plugin -->
+    <link href="{{ asset('tabler/assets/plugins/maps-google/plugin.css') }}" rel="stylesheet" />
+    <!-- c3.js Charts Plugin -->
+    <link href="{{ asset('tabler/assets/plugins/charts-c3/plugin.css') }}" rel="stylesheet" />
 </head>
 <body>
     <div id="app">
@@ -71,5 +80,19 @@
             @yield('content')
         </main>
     </div>
+    <script src="{{ asset('tabler/assets/js/require.min.js') }}"></script>
+    <script>
+      requirejs.config({
+          baseUrl: '{{ config('app.url') }}/tabler'
+      });
+    </script>
+    <!-- Dashboard Core -->
+    <script src="{{ asset('tabler/assets/js/dashboard.js') }}"></script>
+    <!-- Google Maps Plugin -->
+    <script src="{{ asset('tabler/assets/plugins/maps-google/plugin.js') }}"></script>
+    <!-- c3.js Charts Plugin -->
+    <script src="{{ asset('tabler/assets/plugins/charts-c3/plugin.js') }}"></script>
+    <!-- Input Mask Plugin -->
+    <script src="{{ asset('tabler/assets/plugins/input-mask/plugin.js') }}"></script>
 </body>
 </html>
